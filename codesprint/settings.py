bbfcaa9e2622aa5eff4c8a264814ae2a2a4df967
browne878 <dragon015789@gmail.com>
2022-06-19 14:17:42 +0000

    Update allowed hosts

--- codesprint/settings.py
+++ codesprint/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pp4-codesprint.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['https://pp4-codesprint.herokuapp.com/', 'localhost']
 
 
 # Application definition
