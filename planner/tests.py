04af890a57cd055daa235c0944b11767447cbe82
browne878 <dragon015789@gmail.com>
2022-06-26 10:00:19 +0000

    Complete testing and final deploy

--- planner/tests.py
+++ planner/tests.py
@@ -12,12 +12,11 @@ class TestViews(TestCase):
         response = self.client.get('/accounts/login/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'account/login.html')
-    
+
     def test_signup_page(self):
         """
-        Checks if the login page is displayed
+        Checks if the signup page is displayed
         """
         response = self.client.get('/accounts/signup/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'account/signup.html')
-
